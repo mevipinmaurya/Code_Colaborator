492618c81d12bed7d5c16b6e7fd88c175967263d
mevipinmaurya <mevipin2212001@gmail.com>
2024-10-09 00:21:15 +0530

    Made responsive

--- Client/src/component/Editorpage.js
+++ Client/src/component/Editorpage.js
@@ -197,84 +197,80 @@ function Editorpage() {
 
 
   return (
-    <div className='container-fluid vh-100'>
-      <div className='row h-100 '>
-        {/* left section of editor page */}
-        <div className='col-md-2 bg-dark text-light d-flex flex-column h-100 ' style={{ boxShadow: "2px 0px 4px rgba(0,0,0,0.1)" }}>
-          <img className='img-fluid mx-auto d-block mb-3 mt-3' src="/images/logo.png" style={{ maxWidth: '120px', marginTop: "0px" }} />
-          <hr style={{ marginTop: "-0rem" }} />
-          {/* client list container */}
-          <div className='d-flex flex-column  overflow-auto'>
-            {/* client add krna hai */}
-            {clients.map((client) => (
-              <Client key={client.socketid} username={client.username} />
-            ))}
-          </div>
+    <>
+      <div className='container-fluid vh-100'>
+        <div className='row '>
 
-          <div className='mt-auto'>
-            <hr style={{}} />
-            {/* <button onClick={copyRoomId} className='btn btn-success btn-block'>Copy RoomId</button>
-            <button onClick={leaveRoom} className='btn btn-danger mt-2 mb-2 px-3 btn-block'>Leave Room</button> */}
-
-            {/* <p className='text-center text-secondary'>All rights are reserved to CodeTogether.</p> */}
-            <p className='text-center text-secondary'>Created by Vipin and Shubham</p>
-          </div>
-        </div>
-
-        {/* right section of editor page */}
-        <div className='col-md-10  text-light d-flex flex-column ' >
-
-          <div className='w-full py-2 px-2 bg-dark d-flex justify-content-between'>
-
-            <div className='w-1/3'>
-              <select id="mySelect" value={selectedValue} onChange={handleChange} className="form-select" aria-label="Default select example">
-                <option value="Java">Java</option>
-                <option value="Cpp">C++</option>
-                <option value="C">C</option>
-                <option value="Python">Python</option>
-              </select>
+          <div className='col-md-2 bg-dark text-light d-flex flex-column' style={{ boxShadow: "2px 0px 4px rgba(0,0,0,0.1)" }}>
+            <img className='img-fluid mx-md-auto d-block mb-3 mt-3' src="/images/logo.png" style={{ maxWidth: '120px', marginTop: "0px" }} />
+            <hr style={{ marginTop: "-0rem" }} />
+            <div className='d-flex flex-column overflow-auto mb-3'>
+              {clients.map((client) => (
+                <Client key={client.socketid} username={client.username} />
+              ))}
             </div>
 
-            <div className='cursor-pointer d-flex mx-3'>
-
-              <div className='mx-3 text-danger my-auto' style={{ cursor: "pointer" }}>
-                <div onClick={toggleMic} data-toggle="tooltip" data-placement="top" title={`${mic ? "Mic Off" : "Mic On"}`}>
-                  {mic ?
-                    <AiFillAudio size={"30px"} className='d-sm-inline' />
-                    :
-                    <AiOutlineAudioMuted style={{ color: "green" }} size={"30px"} />
-                  }
-                </div>
-              </div>
-
-              {/* Features that can be implemented */}
-              <div className='d-none d-sm-block mx-3 text-danger my-auto' style={{ cursor: "pointer" }}>
-                <div onClick={handleToggleVideo} data-toggle="tooltip" data-placement="top" title={`${videoOn ? "Video Off" : "Video On"}`}>
-                  {videoOn ?
-                    <FaVideo style={{ font: "bold" }} size={"30px"} />
-                    :
-                    <CiVideoOff style={{ color: "green" }} size={"30px"} />
-                  }
-                </div>
-              </div>
-
-              <div className='mx-3 text-success' style={{ cursor: "pointer" }}>
-                <button type="button" className='btn btn-success' data-toggle="tooltip" data-placement="top" title="Copy RoomId" onClick={copyRoomId}>
-                  <MdOutlineContentCopy size={"25px"} />
-                </button>
-              </div>
-              <div className='mx-3 text-danger' style={{ cursor: "pointer" }}>
-                <button type="button" className='btn btn-danger' data-toggle="tooltip" data-placement="top" title="Leave Room" onClick={leaveRoom}>
-                  <RiLogoutCircleLine size={"25px"} />
-                </button>
-              </div>
+            <div className='mt-auto d-none d-md-block'>
+              <hr style={{}} />
+              <p className='text-center text-secondary'>Created by Vipin and Shubham</p>
             </div>
           </div>
 
-          < Editor socketRef={socketRef} lang={selectedValue} roomId={roomId} onCodeChange={(code) => codeRef.current = code} />
+
+          <div className='col-md-10  text-light d-flex flex-column ' >
+
+            <div className='w-full py-2 px-2 bg-dark d-flex justify-content-between'>
+
+              <div className='w-1/3' style={{width: "160px"}}>
+                <select id="mySelect" value={selectedValue} onChange={handleChange} className="form-select" aria-label="Default select example">
+                  <option value="Java">Java</option>
+                  <option value="Cpp">C++</option>
+                  <option value="C">C</option>
+                  <option value="Python">Python</option>
+                </select>
+              </div>
+
+              <div className='cursor-pointer d-flex mx-3'>
+
+                <div className='mx-3 text-danger my-auto' style={{ cursor: "pointer" }}>
+                  <div onClick={toggleMic} data-toggle="tooltip" data-placement="top" title={`${mic ? "Mic Off" : "Mic On"}`}>
+                    {mic ?
+                      <AiFillAudio size={"25px"} className='d-sm-inline' />
+                      :
+                      <AiOutlineAudioMuted style={{ color: "green" }} size={"25px"} />
+                    }
+                  </div>
+                </div>
+
+
+                <div className='d-none d-sm-block mx-3 text-danger my-auto' style={{ cursor: "pointer" }}>
+                  <div onClick={handleToggleVideo} data-toggle="tooltip" data-placement="top" title={`${videoOn ? "Video Off" : "Video On"}`}>
+                    {videoOn ?
+                      <FaVideo style={{ font: "bold" }} size={"25px"} />
+                      :
+                      <CiVideoOff style={{ color: "green" }} size={"25px"} />
+                    }
+                  </div>
+                </div>
+
+                <div className='mx-3 text-success' style={{ cursor: "pointer" }}>
+                  <button type="button" className='btn btn-success' data-toggle="tooltip" data-placement="top" title="Copy RoomId" onClick={copyRoomId}>
+                    <MdOutlineContentCopy size={"20px"} />
+                  </button>
+                </div>
+                <div className='mx-2 text-danger' style={{ cursor: "pointer" }}>
+                  <button type="button" className='btn btn-danger' data-toggle="tooltip" data-placement="top" title="Leave Room" onClick={leaveRoom}>
+                    <RiLogoutCircleLine size={"20px"} />
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            < Editor socketRef={socketRef} lang={selectedValue} roomId={roomId} onCodeChange={(code) => codeRef.current = code} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
